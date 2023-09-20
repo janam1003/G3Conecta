382a5fe7681378a41456772196850994d6bd5cd9
dimoDani <danidiazmoka@gmail.com>
2023-09-20 10:40:26 +0200

    First version of the development of my methods

--- src/Model/DAOImplementationDB.java
+++ src/Model/DAOImplementationDB.java
@@ -4,6 +4,8 @@ import Classes.ConvocatoriaExamen;
 import Classes.Enunciado;
 import Classes.UnidadDidactica;
 import Exceptions.ExceptionManager;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 
 /**
  *
@@ -28,7 +30,28 @@ public class DAOImplementationDB implements DAO {
 
     @Override
     public void ConsultUnidadDidactica(UnidadDidactica unidadDidactica) throws ExceptionManager {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        private Connection con;
+	private PreparedStatement stmt;
+	private OpenCloseConnection occ = new OpenCloseConnection();
+        final String queryUnidad = "Select id from unidad where id=?";
+
+
+		con = occ.openConnection();
+		try {
+			stmt = con.prepareStatement(queryUnidad);
+			stmt.setString(1, unidadDidactica.getId());
+			ResultSet rs = stmt.executeQuery();
+			while (rs.next()) {
+				id = rs.getInt("user_id");
+			}
+		} catch (SQLException e) {
+			String error = "Error logging in";
+			MyException er = new MyException(error);
+			throw er;
+
+		}
+		occ.closeConnection(stmt, con);
+		return unidadDidactida;
     }
 
     @Override
