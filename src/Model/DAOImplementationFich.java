f61b71d7d12ecaf60adcde710747d75479f084a6
dimoDani <danidiazmoka@gmail.com>
2023-09-22 08:57:59 +0200

    Second version of my methods

--- src/Model/DAOImplementationFich.java
+++ src/Model/DAOImplementationFich.java
@@ -4,6 +4,11 @@ import Classes.ConvocatoriaExamen;
 import Classes.Enunciado;
 import Classes.UnidadDidactica;
 import Exceptions.ExceptionManager;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import utils.Util;
 
 /**
  *
@@ -32,9 +37,36 @@ public class DAOImplementationFich implements DAO {
     }
 
     @Override
-    public void ConsultConvocatoriaExamen(ConvocatoriaExamen convocatoriaExamen) throws ExceptionManager {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+    public boolean ConsultConvocatoriaExamen(ConvocatoriaExamen convocatoriaExamen) throws ExceptionManager {
+		// TODO Auto-generated method stub
+                boolean esta = false;
+		String wTelefono = null;
+		int cuantos = 0;
+		if (fich.exists()) {
+			cuantos = Util.calculoFichero(fich);
+			try {
+				FileInputStream fis = new FileInputStream(fich);
+				ObjectInputStream ois = new ObjectInputStream(fis);
+				for (int i = 0; i < cuantos; i++) {
+					ConvocatoriaExamen convocatoriaExamen2 = (ConvocatoriaExamen) ois.readObject();
+					if (convocatoriaExamen.getId() == convocatoriaExamen2.getId()) {
+						esta=true;
+					}
+				}
+				ois.close();
+			} catch (FileNotFoundException e) {
+				System.out.println("File not found");
+			} catch (IOException e) {
+				System.out.println("Error IO");
+			} catch (ClassNotFoundException e) {
+				System.out.println("Error en el tipo de datos");
+			}
+
+		} else {
+			System.out.println("File not found");
+		}
+                return esta;
+	}
 
     @Override
     public void ConsultConvocatoriasUD(ConvocatoriaExamen convocatoriaExamen) throws ExceptionManager {
